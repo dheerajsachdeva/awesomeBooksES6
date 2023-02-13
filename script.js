1f743a70cae538c3a9fbd71d212feca27e2ff5c4
veronica365 <naruth10@gmail.com>
2023-02-13 19:54:33 +0300

    Add changes to fix ESlint issues from linters

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-let getBooks = JSON.parse(localStorage.getItem('books'));
+const getBooks = JSON.parse(localStorage.getItem('books'));
 let books = getBooks || [];
 
+const formText = document.querySelector('#articleForm');
 
-const formText = document.querySelector("#articleForm");
+function displayData() {
+  let innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+    innerHTML += `
+            <article id="${books[i].length + 1}">
+                <div class="title">${books[i].title}</div>
+                <div class="author">${books[i].author}</div>
+                <button class="removeButton" onclick="removeData(${
+  books[i].id
+})">Remove</button>
+            </article>
+            <hr />
+            `;
+  }
+  document.querySelector('#articles').innerHTML = innerHTML;
+}
 
-const title = document.querySelector(".title");
-const author = document.querySelector(".author");
-
-const addButton = document.querySelector(".addButton");
-const removeButton = document.querySelector(".removeButton");
-
-formText.addEventListener("submit", (event) => {
+formText.addEventListener('submit', (event) => {
   event.preventDefault();
   const titleText = formText.elements.titleText.value;
   const authorText = formText.elements.authorText.value;
@@ -19,54 +29,33 @@ formText.addEventListener("submit", (event) => {
       id: books.length + 1,
       title: titleText,
       author: authorText,
-          });
-  let myBooks = JSON.stringify(books);
-localStorage.setItem('books', myBooks);
+    });
+    const myBooks = JSON.stringify(books);
+    localStorage.setItem('books', myBooks);
 
     displayData();
-    formText.elements.titleText.value = "";
-    formText.elements.authorText.value = "";
+    formText.elements.titleText.value = '';
+    formText.elements.authorText.value = '';
   }
 });
 
-function displayData() {
-  let innerHTML = "";
-  for (let i = 0; i < books.length; i += 1) {
-    innerHTML += `
-            <article id="${books[i].length + 1}">
-                <div class="title">${books[i].title}</div>
-                <div class="author">${books[i].author}</div>
-                <button class="removeButton" onclick="removeData(${
-                  books[i].id
-                })">Remove</button>
-            </article>
-            <hr />
-            `;
-  }
-  document.querySelector("#articles").innerHTML = innerHTML;
-}
-
+/* eslint-disable no-unused-vars */
 function removeData(id) {
   books = books.filter((book) => id !== book.id);
-  let myBooks = JSON.stringify(books);
-localStorage.setItem('books', myBooks);
+  const myBooks = JSON.stringify(books);
+  localStorage.setItem('books', myBooks);
   displayData();
 }
 displayData();
 
-
 const storageForm = document.querySelector('#articleForm');
 storageForm.addEventListener('input', () => {
- 
-console.log(getBooks);
   const data = {
     titleText: formText.elements.titleText.value,
     authorText: formText.elements.authorText.value,
-};
+  };
   localStorage.setItem('storageForm', JSON.stringify(data));
 });
 const formObject = JSON.parse(localStorage.getItem('storageForm'));
 formText.elements.titleText.value = formObject.titleText;
 formText.elements.authorText.value = formObject.authorText;
-
-
